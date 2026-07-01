953423904c89144b88e50ec492f4d95c1e01b0ce
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-07-01 15:55:11 +1000

    cleanup

--- src/rd_qc/jobs/generate_somalier.py
+++ src/rd_qc/jobs/generate_somalier.py
@@ -1,8 +1,3 @@
-from hailtop.batch.job import BashJob
-
-from cpg_flow.status import complete_analysis_job
-from cpg_utils import to_path, Path
-
 """
 Job to generate somalier fingerprints for SGs missing them.
 Runs somalier extract on each input file and registers the result in metamist.
@@ -84,10 +79,3 @@ def somalier_jobs(
 
     return jobs
 
-#def somalier_jobs(somalier_targets: dict[str, Path]) -> list[BashJob]:
-#    """
-#    Method to take all the somalier targets, decide what type of index each has (tbi, crai) based on file type.
-#    localises the main file and index inside a new somalier job, runs somalier extract
-#    writes the new file to f'{input}.somalier'
-#    registers the result using complete_analysis_job, writing a single-SG ID 'somalier' analysis type
-#    """
