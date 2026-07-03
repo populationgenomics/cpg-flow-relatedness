1089e97a36704aa60b70a09168e86dccc62861c8
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-07-03 19:25:38 +1000

    meta test rego

--- src/rd_qc/jobs/generate_somalier.py
+++ src/rd_qc/jobs/generate_somalier.py
@@ -76,13 +76,17 @@ def somalier_jobs(
             attributes={'tool': 'metamist'},
         )
         registration_job.image(config.config_retrieve(['workflow', 'driver_image']))
+        metamist_project = project
+        if config.config_retrieve(['workflow', 'access_level']) == 'test' and 'test' not in project:
+            metamist_project = f'{project}-test'
+
         registration_job.call(
             register_analyses,
             output=str(output_path),
             analysis_type='somalier',
             cohort_ids=[],
             sg_ids=[sg_id],
-            project_name=project,
+            project_name=metamist_project,
             meta={},
         )
         registration_job.depends_on(j)
