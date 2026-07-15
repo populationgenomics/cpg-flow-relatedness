8569d227df2956086d7015f48660e4f98caa9162
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-07-15 13:42:47 +1000

    lint

--- src/rd_qc/utils.py
+++ src/rd_qc/utils.py
@@ -129,7 +129,7 @@ def _select_best_file_for_sg(analyses: list[dict]) -> str | None:
     """
     priority = config_retrieve(
         ['workflow', 'somalier_extract', 'priority'],
-        ['gvcf', 'cram'],
+        ['cram', 'gvcf'],
     )
 
     buckets: dict[str, list[str]] = {'cram': [], 'gvcf': []}
